4ec61489a3fb0b5feec9c7ec54bb602279278c07
shujaatk92 <154977983+shujaatk92@users.noreply.github.com>
2023-12-30 12:03:46 +0000

    logo

--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 import { Card, CardContent } from "./ui/card";
 import { MAX_FREE_COUNT } from "@/constants";
+import { Progress } from "./ui/progress";
+import { Button } from "./ui/button";
+import { Zap } from "lucide-react";
 
 interface FreeCountProps {
     apiLimitCount: number;
@@ -28,7 +31,12 @@ export const FreeCounter = ({apiLimitCount = 0}: FreeCountProps ) => {
                         <p>
                             {apiLimitCount} / {MAX_FREE_COUNT} Free Generations
                         </p>
+                        <Progress className="h-3" value={(apiLimitCount/ MAX_FREE_COUNT) * 100}/>
                     </div>
+                    <Button className="w-full" variant="permium">
+                        Upgrade
+                        <Zap className="w-4 h-4 ml-2 fill-white"/>
+                    </Button>
                 </CardContent>
             </Card>
         </div>
